19dbdfccdaf9a0e883687b0fe42dc4d2464099ac
Eric Bris <cubaneric01@gmail.com>
2019-03-03 13:44:00 -0600

    Defining positions

--- main.go
+++ main.go
@@ -16,10 +16,25 @@ var (
 	// Use mcu pin 10, corresponds to physical pin 19 on the pi
 	pin = rpio.Pin(14)
         pin1 = rpio.Pin(15)
+        currentPosition=-1
+)
+const(
+        POSITION1=1
+        POSITION2=2
+        POSITION3=3
+        POSITION4=4
+        POSITION5=5
+        RESET=6
+        POSITIONTIME=3
 )
 
 
 func main() {
+    if currentPosition==-1{
+    //read info from file and setup "currentPosition" var
+    //in case the file does not exist or it is empty the var should be set to 0
+    currentPosition=0 //this should change later when reading from the file. this is a temp line
+    }
     defineEndpoints()
 }
 
@@ -96,7 +111,7 @@ jsonReturn := map[string]interface{}{"version": "1.0","shouldEndSession": true}
        json.NewEncoder(w).Encode(jsonReturn)
 }
 
-func position1(w http.ResponseWriter, r * http.Request){
+func position(w http.ResponseWriter, r * http.Request){
    vars := mux.Vars(r)
    position := vars["number"]
    var err error
@@ -107,8 +122,18 @@ func position1(w http.ResponseWriter, r * http.Request){
    }
    var positionInt int
    positionInt = int(tempPosition)
-   on(w,r)
-   time.Sleep(time.Duration(positionInt)*2*time.Second)
+   positionMovement := determineTableMovement(positionInt)
+
+   currentPosition = currentPosition+positionMovement
+   if positionMovement > 0 {
+       on(w,r)
+       time.Sleep(time.Duration(positionMovement)*POSITIONTIME*time.Second)
+   } else {
+       off(w,r)
+       positionMovement = -1*positionMovement
+       time.Sleep(time.Duration(positionMovement)*POSITIONTIME*time.Second)
+   }
+   
    stop(w,r)
    jsonReturn := map[string]interface{}{"version":"1.0"}
    json.NewEncoder(w).Encode(jsonReturn)
@@ -121,10 +146,21 @@ func defineEndpoints() {
     router.HandleFunc("/up", on).Methods("GET")
     router.HandleFunc("/down", off).Methods("GET")
     router.HandleFunc("/stop", stop).Methods("GET")
-    router.HandleFunc("/position/{number}",position1).Methods("GET")
+    router.HandleFunc("/position/{number}",position).Methods("GET")
 
     //router.HandleFunc("/", rootMethod).Methods("POST")
     log.Fatal(http.ListenAndServe(":8070",router))
 
 
 } 
+
+//this function determine how many positions the table has to be move and the way. Positive goes up and negative goes down
+func determineTableMovement(goTo int) int{
+    if goTo==POSITION1 || goTo==POSITION2 || goTo==POSITION3 || goTo==POSITION4 || goTo==POSITION5 || goTo==RESET{
+        if (goTo==RESET) {
+            return -1*currentPosition
+        }
+        return goTo-currentPosition
+    }
+    return 0
+}
